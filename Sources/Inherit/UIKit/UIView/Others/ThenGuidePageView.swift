719fa7c19a4c073ebf03e3ccd8cc6d5fd1c8e994
ghost <czios1501@gmail.com>
2023-11-17 16:58:16 +0800

    feat: fix package

--- Sources/Inherit/UIKit/UIView/Others/ThenGuidePageView.swift
+++ Sources/Inherit/UIKit/UIView/Others/ThenGuidePageView.swift
@@ -5,10 +5,10 @@
 //  Created by ghost on 2023/3/10.
 //
 
-import UIKit
 import ThenFoundation
+import UIKit
 
-extension ThenGuidePageView {
+public extension ThenGuidePageView {
     /**
      skipTitle: 跳过按钮的标题
      imageNames: 本地图片名称
@@ -16,11 +16,13 @@ extension ThenGuidePageView {
      force: 是否强制展示，不管是不是第一次
      dismiss: 消失前的回调
      */
-    public class func show(skip title: String? = "立即体验",
-                           images imageNames: [String]?,
-                           in view: UIView,
-                           force forceShow: Bool = false,
-                           _ dismiss: (() -> ())?) -> Bool {
+    class func show(
+        skip title: String? = "立即体验",
+        images imageNames: [String]?,
+        in view: UIView,
+        force forceShow: Bool = false,
+        _ dismiss: (() -> Void)?
+    ) -> Bool {
         guard let names = imageNames else {
             return false
         }
@@ -42,24 +44,24 @@ extension ThenGuidePageView {
     }
 }
 
-@IBDesignable
-open class ThenGuidePageView: UIView {
-    
+// MARK: - ThenGuidePageView
+
+@IBDesignable open class ThenGuidePageView: UIView {
     static let storeKey = "com.then.park.guide"
-    
+
     /// 留边
     @IBInspectable
-    open var contentInsets: UIEdgeInsets = UIEdgeInsets.zero
+    open var contentInsets: UIEdgeInsets = .zero
     /// 间距
     @IBInspectable
-    open var gap: CGSize = CGSize.zero
-    
+    open var gap: CGSize = .zero
+
     /// 跳过按钮的标题
     open var skipTitle: String? {
         get { enter.title(for: .normal) }
         set { enter.setTitle(newValue, for: .normal) }
     }
-    
+
     /// 本地图片名称
     open var imageNames: [String] = [] {
         didSet {
@@ -67,29 +69,29 @@ open class ThenGuidePageView: UIView {
             collectionView.reloadData()
         }
     }
-    
+
     /// 消失前的回调
     open var dismiss: (() -> Void)?
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    public override init(frame: CGRect) {
+
+    override public init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(collectionView)
-        addSubview(pageControl)
-        addSubview(enter)
+        addSubview(self.collectionView)
+        addSubview(self.pageControl)
+        addSubview(self.enter)
     }
-    
+
     /// 重新布局
-    open override func layoutSubviews() {
+    override open func layoutSubviews() {
         super.layoutSubviews()
-        collectionView.frame = bounds
-        pageControl.center = CGPoint(x: bounds.then.centerX, y: bounds.height - 78)
-        enter.center = CGPoint(x: bounds.then.centerX, y: pageControl.then.centerY)
+        self.collectionView.frame = bounds
+        self.pageControl.center = CGPoint(x: bounds.then.centerX, y: bounds.height - 78)
+        self.enter.center = CGPoint(x: bounds.then.centerX, y: self.pageControl.then.centerY)
     }
-    
+
     /// 表格视图
     open lazy var collectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
@@ -108,18 +110,18 @@ open class ThenGuidePageView: UIView {
         cv.then.registerClassWithCell(Cell.self)
         return cv
     }()
-    
+
     /// 翻页指示器
     open lazy var pageControl: PageControl = {
         let view = PageControl(frame: CGSize(width: 300, height: 12).then.bounds)
         view.backgroundColor = UIColor.clear
         return view
     }()
-    
+
     /// 进入按钮
     open lazy var enter: UIButton = {
         var button = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 48))
-        button.backgroundColor = UIColor(hex: 0x8b49f6)
+        button.backgroundColor = UIColor(hex: 0x8B49F6)
         button.setTitleColor(UIColor.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         button.setTitle("立即体验", for: .normal)
@@ -132,7 +134,7 @@ open class ThenGuidePageView: UIView {
         button.alpha = 0
         return button
     }()
-    
+
     /// 进入按钮的触发事件
     open func enterButtonAction(_ button: UIButton) {
         if let version = Bundle.main.then.version {
@@ -141,47 +143,49 @@ open class ThenGuidePageView: UIView {
         }
         UIView.animate(withDuration: 0.2, animations: {
             self.alpha = 0
-        }, completion: { (finished) in
+        }, completion: { finished in
             self.dismiss?()
             self.removeFromSuperview()
         })
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
+
 extension ThenGuidePageView: UICollectionViewDelegateFlowLayout {
-    
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return then.size
     }
 }
 
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+
 extension ThenGuidePageView: UICollectionViewDataSource, UICollectionViewDelegate {
-    
     open func scrollViewDidScroll(_ scrollView: UIScrollView) {
         var offset = scrollView.contentOffset
         offset.x = min(scrollView.contentSize.width - scrollView.then.width, max(0, offset.x))
         scrollView.contentOffset = offset
-        
+
         let ix = 0.5 + offset.x / scrollView.then.width
-        pageControl.index = Int(ix)
-        
+        self.pageControl.index = Int(ix)
+
         let cut = 1.5 * scrollView.then.width
         if offset.x < (scrollView.contentSize.width - cut) {
-            enter.isHidden = true
+            self.enter.isHidden = true
             return
         }
         let less = offset.x - cut
         let p = less / (scrollView.then.width / 2.0)
-        
-        enter.isHidden = false
-        pageControl.alpha = 1.0 - p
-        enter.alpha = p
+
+        self.enter.isHidden = false
+        self.pageControl.alpha = 1.0 - p
+        self.enter.alpha = p
     }
-    
+
     open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageNames.count
+        return self.imageNames.count
     }
-    
+
     open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.then.dequeueCell(Cell.self, for: indexPath)
         if let imageNamed = imageNames[safe: indexPath.item] {
@@ -191,42 +195,44 @@ extension ThenGuidePageView: UICollectionViewDataSource, UICollectionViewDelegat
     }
 }
 
-public extension ThenGuidePageView {
+// MARK: - ThenGuidePageView.PageControl
 
+public extension ThenGuidePageView {
     @IBDesignable
     class PageControl: UIView {
-        
         /// 留边
         @IBInspectable
-        open var contentInsets: UIEdgeInsets = UIEdgeInsets.zero
+        open var contentInsets: UIEdgeInsets = .zero
         /// 间距
         @IBInspectable
-        open var gap: CGSize = CGSize.zero
-        
-        public struct Block {
-            public static var color = UIColor(hex: 0xcccccc)
-            public static var tintColor = UIColor(hex: 0x8b49f6)
+        open var gap: CGSize = .zero
+
+        public enum Block {
+            public static var color = UIColor(hex: 0xCCCCCC)
+            public static var tintColor = UIColor(hex: 0x8B49F6)
             public static var size: CGSize = 8.then.size
         }
-        
+
         public var totalCount: Int = 0 {
             didSet {
-                blockViews.forEach { (vw) in vw.removeFromSuperview() }
-                blockViews.removeAll()
-                totalCount.then.repeating { _ in
+                self.blockViews.forEach { vw in vw.removeFromSuperview() }
+                self.blockViews.removeAll()
+                self.totalCount.then.repeating { _ in
                     let v = UIView(frame: Block.size.then.bounds)
                     v.backgroundColor = Block.color
                     addSubview(v)
-                    blockViews.append(v)
+                    self.blockViews.append(v)
                 }
-                index = min(max(0, totalCount - 1), index)
+                self.index = min(max(0, self.totalCount - 1), self.index)
                 setNeedsLayout()
             }
         }
-        
+
         public var index: Int = -1 {
             didSet {
-                if oldValue == index { return }
+                if oldValue == self.index {
+                    return
+                }
                 if let oldVw = blockViews[safe: oldValue] {
                     UIView.animate(withDuration: 0.2, animations: {
                         oldVw.backgroundColor = Block.color
@@ -241,50 +247,51 @@ public extension ThenGuidePageView {
                 }
             }
         }
-        
+
         public var blockViews = [UIView]()
-        
+
         // MARK: Life cycle
+
         public required init?(coder aDecoder: NSCoder) {
             super.init(coder: aDecoder)
         }
-        
-        public override init(frame: CGRect) {
+
+        override public init(frame: CGRect) {
             super.init(frame: frame)
-            gap = CGSize(width: 12, height: 0)
+            self.gap = CGSize(width: 12, height: 0)
         }
-        
-        public override func layoutSubviews() {
+
+        override public func layoutSubviews() {
             super.layoutSubviews()
-            let inFrame = bounds.inset(by: contentInsets)
-            let totalWidth = gap.width * CGFloat(max(0, totalCount - 1)) + CGFloat(totalCount) * Block.size.width
+            let inFrame = bounds.inset(by: self.contentInsets)
+            let totalWidth = self.gap.width * CGFloat(max(0, self.totalCount - 1)) + CGFloat(self.totalCount) * Block.size.width
             var center_x = inFrame.then.left + (inFrame.width - totalWidth) / 2.0 + Block.size.width / 2
-            blockViews.forEach {
+            self.blockViews.forEach {
                 $0.center = CGPoint(x: center_x, y: inFrame.then.centerY)
-                center_x += (gap.width + Block.size.width)
+                center_x += (self.gap.width + Block.size.width)
             }
         }
     }
 }
 
+// MARK: - ThenGuidePageView.Cell
+
 public extension ThenGuidePageView {
-    
-    class Cell : UICollectionViewCell {
-        
+    class Cell: UICollectionViewCell {
         public required init?(coder aDecoder: NSCoder) {
             super.init(coder: aDecoder)
         }
-        
-        public override init(frame: CGRect) {
+
+        override public init(frame: CGRect) {
             super.init(frame: frame)
-            addSubview(imageView)
+            addSubview(self.imageView)
         }
-        
-        public override func layoutSubviews() {
+
+        override public func layoutSubviews() {
             super.layoutSubviews()
-            imageView.frame = bounds
+            self.imageView.frame = bounds
         }
-        
-        public lazy var imageView: UIImageView = { UIImageView() }()
+
+        public lazy var imageView: UIImageView = .init()
     }
 }
--- Sources/Inherit/UIKit/UIView/Others/ThenSegmentControl.swift
+++ Sources/Inherit/UIKit/UIView/Others/ThenSegmentControl.swift
@@ -5,176 +5,179 @@
 //  Created by ghost on 2020/3/12.
 //
 
-import UIKit
 import ThenFoundation
+import UIKit
+
+// MARK: - ThenSegmentControl
 
 open class ThenSegmentControl: UIScrollView {
-    
     /// 选中的索引
     public private(set) var selectedIndex: Int = 0
-    
+
     /// 展示可见的数量，可小数
     public var contentCount: CGFloat {
-        get { contentCount_ }
-        set { contentCount_ = newValue; setNeedsLayout() }
+        get { self.contentCount_ }
+        set { self.contentCount_ = newValue; setNeedsLayout() }
     }
-    
+
     /// 要展示的选项
     public var items: [Item] = [] {
         didSet {
-            segments.forEach { $0.removeFromSuperview() }
-            segments = items.compactMap {
+            self.segments.forEach { $0.removeFromSuperview() }
+            self.segments = self.items.compactMap {
                 Button(item: $0).then.on(.touchUpInside) { [weak self] in self?.action($0) }.value
             }
-            segments.forEach { addSubview($0) }
+            self.segments.forEach { addSubview($0) }
             setNeedsLayout()
         }
     }
-    
+
     /// 选中的回调
     public var selected: ((_ button: Button, _ index: Int) -> Bool)?
-    
+
     /// 对应索引的按钮
     public func button(at index: Int) -> Button? {
-        return segments[safe: index]
+        return self.segments[safe: index]
     }
-    
+
     /// 选中指定索引的选项
     public func select(at index: Int, animated: Bool = true) {
-        selectedIndex = index
+        self.selectedIndex = index
         let size = then.size
-        let ctX = min(contentSize.width - size.width, max(0, (CGFloat(selectedIndex) + 0.5) * size.width / contentCount - size.width / 2))
+        let ctX = min(contentSize.width - size.width, max(0, (CGFloat(self.selectedIndex) + 0.5) * size.width / self.contentCount - size.width / 2))
         setContentOffset(CGPoint(x: ctX, y: contentOffset.y), animated: animated)
-        segments.enumerated().forEach {
+        self.segments.enumerated().forEach {
             $0.element.isDidHighlighted = index == $0.offset
         }
     }
-    
+
     /// 绘制边框 (默认 style:bottom, lineWidth:1, lineColor:0x646464)
     public var drawBorder: ((ThenCALayerBorder) -> Void)? = {
         $0.lineWidth = 1 / UIScreen.main.scale
         $0.lineColor = UIColor(hex: 0x646464)
         $0.style = [.bottom]
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    public override init(frame: CGRect) {
+
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         bounces = false
         showsHorizontalScrollIndicator = false
         showsVerticalScrollIndicator = false
     }
-    
-    open override func layoutSubviews() {
+
+    override open func layoutSubviews() {
         super.layoutSubviews()
         let size = then.size
         let count = CGFloat(items.count)
-        contentCount_ = max(1, min(contentCount, count))
-        let buttonSize = CGSize(width: size.width / contentCount, height: size.height)
-        contentSize = CGSize(width: buttonSize.width * count, height: buttonSize.height)
-        segments.enumerated().forEach {
+        self.contentCount_ = max(1, min(self.contentCount, count))
+        let buttonSize = CGSize(width: size.width / self.contentCount, height: size.height)
+        self.contentSize = CGSize(width: buttonSize.width * count, height: buttonSize.height)
+        self.segments.enumerated().forEach {
             $0.element.frame = CGRect(x: buttonSize.width * CGFloat($0.offset), y: 0, width: buttonSize.width, height: buttonSize.height)
         }
         if let closure = drawBorder {
             layer.then.border.make(closure)
         }
     }
-    
-    open override var contentSize: CGSize {
+
+    override open var contentSize: CGSize {
         didSet {
-            if oldValue.equalTo(.zero), !contentSize.equalTo(.zero) {
-                select(at: selectedIndex, animated: false)
+            if oldValue.equalTo(.zero), !self.contentSize.equalTo(.zero) {
+                self.select(at: self.selectedIndex, animated: false)
             }
         }
     }
-    
+
     private var contentCount_: CGFloat = 4
     private var segments: [Button] = []
-    
+
     private func action(_ button: Button) {
-        guard let `selected` = selected, let index = segments.firstIndex(of: button) else { return }
+        guard let selected = selected, let index = segments.firstIndex(of: button) else {
+            return
+        }
         if selected(button, index) {
-            select(at: index, animated: true)
+            self.select(at: index, animated: true)
         }
     }
 }
 
 public extension ThenSegmentControl {
-    
     class Button: UIControl {
-        
         public var isDidHighlighted: Bool = false {
             didSet {
-                titleLabel.isHighlighted = isDidHighlighted
-                imageView.isHighlighted = isDidHighlighted
-                markView.backgroundColor = isDidHighlighted ? item.highlightedColor : item.color
+                self.titleLabel.isHighlighted = self.isDidHighlighted
+                self.imageView.isHighlighted = self.isDidHighlighted
+                self.markView.backgroundColor = self.isDidHighlighted ? self.item.highlightedColor : self.item.color
             }
         }
-        
+
         public var item: Item
-        
+
+        @available(*, unavailable)
         public required init?(coder aDecoder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-        
+
         public init(item: Item) {
             self.item = item
             super.init(frame: .zero)
-            addSubview(imageView)
-            addSubview(titleLabel)
-            addSubview(markView)
-            markView.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi / 4)
+            addSubview(self.imageView)
+            addSubview(self.titleLabel)
+            addSubview(self.markView)
+            self.markView.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi / 4)
         }
-        
-        public override func layoutSubviews() {
+
+        override public func layoutSubviews() {
             super.layoutSubviews()
-            imageView.frame = bounds
-            titleLabel.frame = bounds.inset(by: item.titleEdgeInset)
-            markView.center = CGPoint(x: titleLabel.then.centerX, y: then.height - 7)
-            
+            self.imageView.frame = bounds
+            self.titleLabel.frame = bounds.inset(by: self.item.titleEdgeInset)
+            self.markView.center = CGPoint(x: self.titleLabel.then.centerX, y: then.height - 7)
         }
-        
+
         public lazy var titleLabel: UILabel = {
             let label = UILabel()
             label.textAlignment = .center
             label.adjustsFontSizeToFitWidth = true
-            label.text = item.title
-            label.textColor = item.color
-            label.highlightedTextColor = item.highlightedColor
-            label.font = item.titleFont
+            label.text = self.item.title
+            label.textColor = self.item.color
+            label.highlightedTextColor = self.item.highlightedColor
+            label.font = self.item.titleFont
             return label
         }()
-        
+
         public lazy var imageView: UIImageView = {
             let image = UIImageView()
-            image.image = item.image
-            image.highlightedImage = item.highlightedImage
+            image.image = self.item.image
+            image.highlightedImage = self.item.highlightedImage
             return image
         }()
-        
-        public lazy var markView: UIView = { UIView(frame: 4.then.square) }()
+
+        public lazy var markView: UIView = .init(frame: 4.then.square)
     }
-    
+
     struct Item {
         public var title: String
         public var color: UIColor
         public var highlightedColor: UIColor
         public var titleFont: UIFont
         public var titleEdgeInset: UIEdgeInsets
-        
+
         public var image: UIImage?
         public var highlightedImage: UIImage?
-        
-        public init(title: String,
-                    color: UIColor = UIColor.black,
-                    highlightedColor: UIColor = UIColor.gray,
-                    titleFont: UIFont = UIFont.systemFont(ofSize: 14),
-                    titleEdgeInset: UIEdgeInsets = .zero,
-                    image: UIImage? = nil,
-                    highlightedImage: UIImage? = nil) {
+
+        public init(
+            title: String,
+            color: UIColor = UIColor.black,
+            highlightedColor: UIColor = UIColor.gray,
+            titleFont: UIFont = UIFont.systemFont(ofSize: 14),
+            titleEdgeInset: UIEdgeInsets = .zero,
+            image: UIImage? = nil,
+            highlightedImage: UIImage? = nil
+        ) {
             self.title = title
             self.color = color
             self.highlightedColor = highlightedColor
